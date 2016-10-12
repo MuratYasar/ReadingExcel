616f59e8d92408f6134f5a0b6c9421bc9726d1ee
Murat Yasar <yasar_murat@hotmail.com>
2016-10-12 14:54:57 +0300

    writing into text file

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,12 +1,13 @@
 import openpyxl
 
 def readexcel():
-    wb = openpyxl.load_workbook(filename="efatura.xlsx")
+    wb = openpyxl.load_workbook(filename="efatura.xlsx", read_only=True)
     ws = wb.get_sheet_by_name('EFatura')
 
-    for cellvalue in range(0, ws.max_row):
-        print(ws.cell(row=cellvalue+1, column=1).value)
-
+    with open("output.txt", "w") as textfile:
+        for row in ws.rows:
+            for cell in row:
+                textfile.write(cell.value + "\n")
 
 if __name__ == '__main__':
     readexcel()
