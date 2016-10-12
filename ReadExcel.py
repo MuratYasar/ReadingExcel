97892554400bd88096e8ae31b3b3db20b433eec8
Murat Yasar <yasar_murat@hotmail.com>
2016-10-12 14:18:02 +0300

    Reading

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,7 +1,12 @@
 import openpyxl
 
 def readexcel():
-    wb = load_workbook(filename=r"C:\Users\Administrator\Desktop\PLM_ENOVIA\urundimensionson.xlsx")
+    wb = openpyxl.load_workbook(filename="efatura.xlsx")
+    ws = wb.get_sheet_by_name('EFatura')
+
+    for cellvalue in range(0, ws.max_row):
+        print(ws.cell(row=cellvalue+1, column=1).value)
+
 
 if __name__ == '__main__':
     readexcel()
