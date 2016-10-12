4a328db4e003eeca68d4768a905705b9b573abad
Murat Yasar <yasar_murat@hotmail.com>
2016-10-12 14:09:14 +0300

    Added sample excel file

---
+++ ReadExcel.py
@@ -0,0 +1,7 @@
+import openpyxl
+
+def readexcel():
+    wb = load_workbook(filename=r"C:\Users\Administrator\Desktop\PLM_ENOVIA\urundimensionson.xlsx")
+
+if __name__ == '__main__':
+    readexcel()
